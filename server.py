534a28ec31e48dfacf50755574fc530fb80a128b
UncleSaurus <unclesaurus@proton.me>
2026-02-03 13:53:09 -0600

    fix: read PORT from environment variable

--- server.py
+++ server.py
@@ -316,4 +316,5 @@ async def stripe_crypto_webhook(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    port = int(os.environ.get("PORT", 8888))
+    uvicorn.run(app, host="0.0.0.0", port=port)
